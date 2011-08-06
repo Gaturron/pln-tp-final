c1cdb30c7ebddb8a35bb5b7997225c7a0826cd26
fernando.bugni <fernando.bugni@de173f2d-743c-7b44-e66f-a61c7830d66a>
2011-08-06 23:18:50 +0000

    agenda mejorada

--- src/DCWrapper/spiders/agenda_spider.py
+++ src/DCWrapper/spiders/agenda_spider.py
@@ -22,109 +22,136 @@ class AgendaSpider(BaseSpider):
         descripcion = hxs.select('//div[@id="content"]/div/dl/dd/span[@class="description"]/text()').extract()          
 	otros = hxs.select('//div[@id="content"]/div/dl/dt/span[@class="contenttype-news-item summary"]/a/text()').extract()
 
-        texto = []
+        textos = []
         index = 0        
         for site in sites:
             item = DcwrapperItem()
-#            titulos = site.extract()
+
             titulo = site.select('span[@class="contenttype-event summary"]/a[@class="state-published url"]/text()').extract()
             lugar = site.select('span[@class="documentByLine"]/span/span[@class="location"]/text()').extract()           
             empieza = site.select('span[@class="documentByLine"]/span/abbr[@class="dtstart"]/text()').extract()          
             termina = site.select('span[@class="documentByLine"]/span/abbr[@class="dtend"]/text()').extract()             
+            url = site.select('span[@class="contenttype-event summary"]/a[@class="state-published url"]/@href').extract()
             
-            #texto.append(str(titulos.encode('utf-8')))
-	    #print 'texto:'
-            texto.append([titulo, lugar, empieza, termina])
+            #analizamos aca titulo para dividir en nombre y tipo
+            titulo = titulo[0]
+            lugar = lugar[0]
+            empieza = empieza[0]
+            termina = termina[0]
+            url = url[0]
+            tipo = ''
+            nombre = ''
+            
+
+            eventos = ['Defensa Tesis Licenciatura', 'Defensa Tesis Doctorado', 'Charla', 'Defensa Tesis', 'Seminario', 'Curso' ]
+            
+            for ev in eventos:
+                if(re.search(ev, str(titulo.encode('utf-8')) )):
+                    tipo = ev
+                    p = re.compile(ev)
+                    nombre = p.sub('', titulo)
+                    p = re.compile(':')
+                    nombre = p.sub('', nombre) 
+                
+            dicc = {'titulo': titulo, 'tipo': tipo, 'nombre': nombre, 'lugar': lugar, 'empieza': empieza, 'termina': termina, 'url': url }
+            textos.append(dicc)            
 
             index = index + 1            
-            #for i in titulos:            
-            #    print 'Title: '+str(i.encode('utf-8'))
-#            for i in materias:
-#                print 'Materias: '+str(i.encode('utf-8'))
-#            for i in materias1:
-#                print 'Materias: '+str(i.encode('utf-8'))
-
-	#print 'descripcion: '+str(descripcion)
-	#print 'otros: '+str(otros)
 
 	#recorremos y juntamos la info
-	for txt in texto[0:14]:
+
+        print textos        
+
+	for txt in textos[0:14]:
 		if(len(descripcion)>0): 
 			descr = descripcion.pop(0)
-			txt.append([descr])
-	texto[14].append([])
-	texto[15].append([descripcion.pop(0)])
-	texto[16].append([])
-	for txt in texto[17:20]:
+			txt['descripcion'] = descr
+	textos[14]['descripcion'] = ''
+	textos[15]['descripcion'] = descripcion.pop(0)
+	textos[16]['descripcion'] = ''
+	for txt in textos[17:20]:
                 if(len(descripcion)>0):
                         descr = descripcion.pop(0)
-                        txt.append([descr])
-	texto[20].append([])
-	for txt in texto[21:27]:
+        		txt['descripcion'] = descr
+	textos[20]['descripcion'] = ''
+	for txt in textos[21:27]:
 	        if(len(descripcion)>0):
         		descr = descripcion.pop(0)
-                	txt.append([descr])
-	texto[27].append([])
-        for txt in texto[28:30]:
+                	txt['descripcion'] = descr
+	textos[27]['descripcion'] = ''
+        for txt in textos[28:30]:
                 if(len(descripcion)>0):
                         descr = descripcion.pop(0)
-                        txt.append([descr])
-        texto[30].append([])
-        for txt in texto[31:60]:
+                	txt['descripcion'] = descr
+	textos[30]['descripcion'] = ''
+        for txt in textos[31:60]:
                 if(len(descripcion)>0):
                         descr = descripcion.pop(0)
-                        txt.append([descr])
+                        txt['descripcion'] = descr
 	descripcion.pop(0)
-        for txt in texto[60:78]:
+        for txt in textos[60:78]:
                 if(len(descripcion)>0):
                         descr = descripcion.pop(0)
-                        txt.append([descr])
-        texto[78].append([])        
-        for txt in texto[79:86]:
+                        txt['descripcion'] = descr
+        textos[78]['descripcion'] = ''        
+        for txt in textos[79:86]:
                 if(len(descripcion)>0):
                         descr = descripcion.pop(0)
-                        txt.append([descr])
-        texto[86].append([])        
-	texto[87].append([])
-        for txt in texto[88:]:
+                        txt['descripcion'] = descr
+        textos[86]['descripcion'] = ''
+	textos[87]['descripcion'] = ''
+        for txt in textos[88:]:
                 if(len(descripcion)>0):
                         descr = descripcion.pop(0)
-                        txt.append([descr])
+                        txt['descripcion'] = descr
 
         #texto dividido
 
         #sacamos espacios al pedo
-        for txt in texto:
-                for frase in txt:
-                        if (len(frase)>0):                
-                                p = re.compile('\'')
-                                frase[0] = p.sub('', frase[0])    
-                                frase[0] = (frase[0].lstrip()).rstrip()
-		
-        for i in texto:
+
+        def sacar_espacios(frase):
+                if (len(frase)>0):                
+                        p = re.compile('\'')
+                        frase = p.sub('', frase)    
+                        frase = (frase.lstrip()).rstrip()
+                        return frase
+                else: return frase        
+        
+        for txt in textos:
+                txt['titulo'] = sacar_espacios(txt['titulo'])
+                txt['tipo'] = sacar_espacios(txt['tipo'])
+                txt['nombre'] = sacar_espacios(txt['nombre'])
+                txt['lugar'] = sacar_espacios(txt['lugar'])
+                txt['termina'] = sacar_espacios(txt['termina'])
+                txt['empieza'] = sacar_espacios(txt['empieza'])
+                txt['descripcion'] = sacar_espacios(txt['descripcion'])
+                txt['url'] = sacar_espacios(txt['url'])
+                         
+        for i in textos:
 		print i
 		print '\n'
 
         #guardemos la informacion
-        #create table agenda (id INTEGER PRIMARY KEY, titulo TEXT, lugar TEXT, empieza TEXT, termina TEXT, descripcion TEXT);
-        self.connection = sqlite3.connect('./database/base1.db')
+        #create table agenda (id INTEGER PRIMARY KEY, titulo TEXT, nombre TEXT, tipo TEXT, lugar TEXT, empieza TEXT, termina TEXT, descripcion TEXT, url TEXT);
+        self.connection = sqlite3.connect('./database/base2.db')
         self.connection.text_factory = str
 
         c = self.connection.cursor()
 
-        for txt in texto:
-                titulo = str( ((txt[0])[0]).encode('utf-8') )
-                lugar = str( ((txt[1])[0]).encode('utf-8') )
-                empieza = str( ((txt[2])[0]).encode('utf-8') )
-                termina = str( ((txt[3])[0]).encode('utf-8') )
+        for txt in textos:
+                titulo = str( (txt['titulo']).encode('utf-8') )
+                tipo = str( (txt['tipo']).encode('utf-8') )
+                nombre = str( (txt['nombre']).encode('utf-8') )      
+                lugar = str( (txt['lugar']).encode('utf-8') )
+                empieza = str( (txt['empieza']).encode('utf-8') )
+                termina = str( (txt['termina']).encode('utf-8') )
+                descripcion = str( (txt['descripcion']).encode('utf-8') )
+                url = str( (txt['url']).encode('utf-8') )
                 
-                if(len(txt[4]) > 0): descripcion = str( ((txt[4])[0]).encode('utf-8') )
-                else: descripcion = ''   
-
-                params = (titulo, lugar, empieza, termina, descripcion, )
-                rows = c.execute('SELECT * FROM agenda WHERE titulo=? AND lugar=? AND empieza=? AND termina=? AND descripcion=? ', params)
+                params = (titulo, tipo, nombre, lugar, empieza, termina, descripcion, url, )
+                rows = c.execute('SELECT * FROM agenda WHERE titulo=? AND tipo=? AND nombre=? AND lugar=? AND empieza=? AND termina=? AND descripcion=? AND url=?', params)
                 if(len(rows.fetchall()) == 0):
-                        s = 'INSERT INTO agenda (titulo, lugar, empieza, termina, descripcion) VALUES(\''+str(titulo)+'\', \''+str(lugar)+'\', \''+str(empieza)+'\', \''+str(termina)+'\', \''+str(descripcion)+'\')'
+                        s = 'INSERT INTO agenda (titulo, tipo, nombre, lugar, empieza, termina, descripcion, url) VALUES(\''+str(titulo)+'\', \''+str(tipo)+'\', \''+str(nombre)+'\', \''+str(lugar)+'\', \''+str(empieza)+'\', \''+str(termina)+'\', \''+str(descripcion)+'\', \''+str(url)+'\')'
                         print s                        
                         c.execute(s)
 
